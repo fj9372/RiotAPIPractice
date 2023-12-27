e7171e1acdba6ef50b46882045b0d3c63957620a
Frankie <hypernova16643@gmail.com>
2023-12-27 18:13:15 -0500

    added average level, placement, players elim, and dmg

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ import time
 import requests
 
 def testMethod(name):
-    api_key = "RGAPI-52ada476-c1b5-4a5b-a755-3344937a916a"
+    api_key = ""
     user_url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + '?api_key=' + api_key
     user = requests.get(user_url).json()
 
@@ -14,10 +14,17 @@ def testMethod(name):
     iconID = user['profileIconId']
     level = user['summonerLevel']         
 
-    matches_url = "https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/" + riotPuuID + '/ids?start=0&count=100&api_key=' + api_key
+    matches_url = "https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/" + riotPuuID + '/ids?start=0&count=200&api_key=' + api_key
+    
     
     matches = requests.get(matches_url).json()
+
     augment_dict = {}
+    level = 0
+    placement = 0
+    players_eliminated = 0
+    total_dmg = 0
+    
     hello = 1
     for i in matches:
         time.sleep(1.5)
@@ -35,7 +42,15 @@ def testMethod(name):
                         augment_dict[augment] += 1
                     else:
                         augment_dict[augment] = 1
+                level += participant['level']
+                placement += participant['placement']
+                players_eliminated += participant['players_eliminated']
+                total_dmg += participant['total_damage_to_players']
     sort_augment = dict(sorted(augment_dict.items(), key = operator.itemgetter(1), reverse=True))
-    print(sort_augment)
+    print(list(sort_augment.keys())[0:3])
+    print("Average Level: ", level/hello)
+    print("Average Placement: ", placement/hello)
+    print("Average Players Eliminated: ", players_eliminated/hello)
+    print("Average Damage: ", total_dmg/hello)
 
 
